2d70a594a3f20efa0c0a1515afa6404387dba27a
jayce <jayce.baxter@proton.me>
2025-03-07 01:48:19 -0500

    init

---
+++ vaccineAssignment/src/Main.java
@@ -0,0 +1,12 @@
+// Program:             Main.java
+// Author:              Jayce Johnson
+// Date:                March 6th, 2025
+// Description:         Retrieves and displays information about
+//                      COVID-19 vaccines.
+
+import java.util.Scanner;
+import java.time.LocalDate;
+
+public class Main {
+
+}
---
+++ vaccineAssignment/src/Vaccine.java
@@ -0,0 +1,9 @@
+public class Vaccine {
+
+    // -	Vaccine ID (int)
+    //-	Vaccine name (str)
+    //-	Cost of 1 unit of vaccine (double)
+    //-	Available units (int)
+    //-	Expiry date (localdate?)
+    //-	Instructions on how to handle the vaccine (str)
+}
