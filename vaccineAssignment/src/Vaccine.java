dd495290c8d53ce4f37c2603b67d081df80e2b93
jayce <jayce.baxter@proton.me>
2025-03-07 01:58:11 -0500

    had intelliJ generate getters and setters love her

--- vaccineAssignment/src/Vaccine.java
+++ vaccineAssignment/src/Vaccine.java
@@ -1,9 +1,72 @@
+// Program:             Vaccine.java
+// Author:              Jayce Johnson
+// Date:                March 6th, 2025
+// Description:         Retrieves and displays information about
+//                      COVID-19 vaccines.
+
+import java.time.LocalDate;
+
+// Vaccine class
 public class Vaccine {
 
-    // -	Vaccine ID (int)
-    //-	Vaccine name (str)
-    //-	Cost of 1 unit of vaccine (double)
-    //-	Available units (int)
-    //-	Expiry date (localdate?)
-    //-	Instructions on how to handle the vaccine (str)
-}
+    // Declaring all variables as private
+    private int vaccineID;
+    private String vaccineName;
+    private double vaccineCost;
+    private int unitsAvail;
+    private LocalDate expiryDate;
+    private String handlingInstructions;
+
+
+    // Getters and setters
+
+    public int getVaccineID() {
+        return vaccineID;
+    }
+
+    public void setVaccineID(int vaccineID) {
+        this.vaccineID = vaccineID;
+    }
+
+    public String getVaccineName() {
+        return vaccineName;
+    }
+
+    public void setVaccineName(String vaccineName) {
+        this.vaccineName = vaccineName;
+    }
+
+    public double getVaccineCost() {
+        return vaccineCost;
+    }
+
+    public void setVaccineCost(double vaccineCost) {
+        this.vaccineCost = vaccineCost;
+    }
+
+    public int getUnitsAvail() {
+        return unitsAvail;
+    }
+
+    public void setUnitsAvail(int unitsAvail) {
+        this.unitsAvail = unitsAvail;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getHandlingInstructions() {
+        return handlingInstructions;
+    }
+
+    public void setHandlingInstructions(String handlingInstructions) {
+        this.handlingInstructions = handlingInstructions;
+    }
+
+    
+} // Closes the vaccine class
